4870611f50e1cd722f78b4033c29c807441a6da0
c4d2 <120351069+c4d2@users.noreply.github.com>
2024-03-25 16:19:34 +0800

    Update index.tsx

--- react-ts-vite-music/src/api/index.tsx
+++ react-ts-vite-music/src/api/index.tsx
@@ -1,25 +1,14 @@
-// 上线部署环境
 
-const SOCKET_URL = "http://47.109.135.84:8000";
+// // 开发环境
+const SOCKET_URL = "http://localhost:8000";
 
-const IMAGE_URL = "http://47.109.135.84:81/images/";
+const IMAGE_URL = "http://localhost:8000/images/";
 
-const UPLOAD_IMAGE = "http://47.109.135.84:81/chatapi/image/upload";
+const UPLOAD_IMAGE = "http://localhost:8000/chatapi/image/upload";
 
-const RequestChat_URL = "http://47.109.135.84:81/chatapi";
+const RequestChat_URL = "http://localhost:8000/chatapi";
 
-const RequestMusic_URL = "http://47.109.135.84:81/api/";
-
-// // // 开发环境
-// const SOCKET_URL = "http://localhost:8000";
-
-// const IMAGE_URL = "http://localhost:8000/images/";
-
-// const UPLOAD_IMAGE = "http://localhost:8000/chatapi/image/upload";
-
-// const RequestChat_URL = "http://localhost:8000/chatapi";
-
-// const RequestMusic_URL = "http://47.109.135.84:81/api/";
+const RequestMusic_URL = "http://localhost:8000/api/";
 
 export {
   SOCKET_URL,
